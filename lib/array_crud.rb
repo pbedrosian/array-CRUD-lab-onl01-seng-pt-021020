aaced7660db2d04c2ce795a8ebc8e7b3553e36dd
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-14 01:07:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,13 @@ end
 
 def add_element_to_end_of_array(array, element)
   new_array = array.push(element)
-
   return new_array
 end
 
 def add_element_to_start_of_array(array, element)
+  new_array = array.unshift(element)
 
+  return new_array
 end
 
 def remove_element_from_end_of_array(array)
